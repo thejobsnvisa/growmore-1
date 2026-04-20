5c59eb8380a9f9291d97560a0d1c962a91b3485e
thejobsnvisa <it@growmore.one>
2026-04-20 11:46:31 +0530

    Integrate ElevenLabs Convai widget in App component
    Added ElevenLabs Convai widget for enhanced interaction.

--- src/App.jsx
+++ src/App.jsx
@@ -203,6 +203,7 @@ function App() {
           element={<IndividualVisaGurdian />}
         />
       </Routes>
+      <elevenlabs-convai agent-id="agent_0001kn90gccre0vazv8wnf4x814y"></elevenlabs-convai><script src="https://unpkg.com/@elevenlabs/convai-widget-embed" async type="text/javascript"></script>
       <Footer />
     </Router>
   );
